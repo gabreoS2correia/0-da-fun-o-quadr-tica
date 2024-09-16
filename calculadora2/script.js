0ac93e7f742a54abb8a6d546a93b951035c780e2
Gabriel Correia <159266902+gabreoS2correia@users.noreply.github.com>
2024-09-16 09:37:24 -0300

    Add files via upload

--- calculadora2/script.js
+++ calculadora2/script.js
@@ -1,13 +1,19 @@
-let delta = (b*b)*(-4*a*c);
-delta = Math.sqrt(delta)
-let x1 = (-b+delta)/(2*a)
-let x2 = (-b-delta)/(2*a)
+function baskhara(){
 
-if(delta>0){
+    let delta = (b*b)-(4*a*c);
+    let rdelta = Math.sqrt(delta);
+    let a = parseFloat(document.getElementById('a').value);
+    let b = parseFloat(document.getElementById('b').value);
+    let c = parseFloat(document.getElementById('c').value);
 
-}else if (delta<0){
-
-}else{
-
-
+    if(delta==0){
+        alert('ta errado');
+    }else if (delta<0){
+        let x1 = (-b)/(2*a);
+        document.getElementById('res').innerText = 'Resultado: ' + x1;
+    }else{
+        let x1 = (-b+rdelta)/(2*a);
+        let x2 = (-b-rdelta)/(2*a);
+        document.getElementById('res').innerText = 'Resultado: ' + x1 + ' e ' + x2;
+    }
 }
